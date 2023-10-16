b3ca71513f4d8649f4cbc00c7ab3e2b6736029ae
miqlar <miquellarsson@gmail.com>
2023-10-16 12:34:11 +0200

    Added test commands

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,32 @@ require("@nomicfoundation/hardhat-toolbox");
 require("uniswap-v3-deploy-plugin");
 require('hardhat-deploy');
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { task } = require("hardhat/config");
+const { execSync } = require("child_process");
+
+
+task("test-v2", "Run V2 tests", async (_, hre) => {
+  try {
+    execSync("npx hardhat test ./test/UniswapV2_basic.js", { stdio: "inherit" });
+  } catch (error) {
+    process.exit(1);
+  }
+  try {
+    execSync("npx hardhat test ./test/UniswapV2_gas_tests.js", { stdio: "inherit" });
+  } catch (error) {
+    process.exit(1);
+  }
+});
+
+task("test-v3", "Run V3 tests", async (_, hre) => {
+  try {
+    execSync("npx hardhat test ./test/UniswapV3_basic.js", { stdio: "inherit" });
+  } catch (error) {
+    process.exit(1);
+  }
+});
+
+
 module.exports = {
   solidity: {
     version: "0.8.19", // Your desired Solidity version
